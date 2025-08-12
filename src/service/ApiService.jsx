7f95f09ffc989a6ef68894a534f65aa54511f450
Celso <celsodiehl@gmail.com>
2025-08-12 18:01:18 -0300

    Criado ApiService dos métodos

---
+++ src/service/ApiService.jsx
@@ -0,0 +1,192 @@
+import axios from "axios"
+
+export default class ApiService {
+
+    static BASE_URL = "http://localhost:8080"
+
+    static getHeader() {
+        const token = localStorage.getItem("token");
+        return {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+        };
+    }
+
+    /**AUTH */
+
+    /* Usado para registrar novo usuário */
+    static async registerUser(registration) {
+        const response = await axios.post(`${this.BASE_URL}/auth/register`, registration)
+        return response.data
+    }
+
+    /* Login de usuário registrado */
+    static async loginUser(loginDetails) {
+        const response = await axios.post(`${this.BASE_URL}/auth/login`, loginDetails)
+        return response.data
+    }
+
+    /***USERS */
+
+
+    /*  Busca Todos usuários */
+    static async getAllUsers() {
+        const response = await axios.get(`${this.BASE_URL}/users/all`, {
+            headers: this.getHeader()
+        })
+        return response.data
+    }
+
+    static async getUserProfile() {
+        const response = await axios.get(`${this.BASE_URL}/users/get-logged-in-profile-info`, {
+            headers: this.getHeader()
+        })
+        return response.data
+    }
+
+
+    /* Busca um usuário */
+    static async getUser(userId) {
+        const response = await axios.get(`${this.BASE_URL}/users/get-by-id/${userId}`, {
+            headers: this.getHeader()
+        })
+        return response.data
+    }
+
+    /* Busca reservas de usuários pelo ID do usuário */
+    static async getUserBookings(userId) {
+        const response = await axios.get(`${this.BASE_URL}/users/get-user-bookings/${userId}`, {
+            headers: this.getHeader()
+        })
+        return response.data
+    }
+
+
+    /* Exclui/Deleta usuário */
+    static async deleteUser(userId) {
+        const response = await axios.delete(`${this.BASE_URL}/users/delete/${userId}`, {
+            headers: this.getHeader()
+        })
+        return response.data
+    }
+
+    /**ROOM */
+    /* Adiciona novo quarto */
+    static async addRoom(formData) {
+        const result = await axios.post(`${this.BASE_URL}/rooms/add`, formData, {
+            headers: {
+                ...this.getHeader(),
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        return result.data;
+    }
+
+    /* Busca todos quartos disponíveis */
+    static async getAllAvailableRooms() {
+        const result = await axios.get(`${this.BASE_URL}/rooms/all-available-rooms`)
+        return result.data
+    }
+
+
+    /* Busca todos os quartos disponíveis por data com uma determinada data e um tipo de quarto */
+    static async getAvailableRoomsByDateAndType(checkInDate, checkOutDate, type) {
+        const result = await axios.get(
+            `${this.BASE_URL}/rooms/available-rooms-by-date-and-type?checkInDate=${checkInDate}
+		&checkOutDate=${checkOutDate}&roomType=${type}`
+        )
+        return result.data
+    }
+
+    /* Busca todos os tipos de quartos */
+    static async getRoomTypes() {
+        const response = await axios.get(`${this.BASE_URL}/rooms/types`)
+        return response.data
+    }
+    /* Busca todos os quartos */
+    static async getAllRooms() {
+        const result = await axios.get(`${this.BASE_URL}/rooms/all`)
+        return result.data
+    }
+    /* Busca quartos por id */
+    static async getRoomById(roomId) {
+        const result = await axios.get(`${this.BASE_URL}/rooms/room-by-id/${roomId}`)
+        return result.data
+    }
+
+    /* Exclui um quarto por Id */
+    static async deleteRoom(roomId) {
+        const result = await axios.delete(`${this.BASE_URL}/rooms/delete/${roomId}`, {
+            headers: this.getHeader()
+        })
+        return result.data
+    }
+
+    /* Atualiza um quarto */
+    static async updateRoom(roomId, formData) {
+        const result = await axios.put(`${this.BASE_URL}/rooms/update/${roomId}`, formData, {
+            headers: {
+                ...this.getHeader(),
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        return result.data;
+    }
+
+
+    /**BOOKING */
+    /* This  saves a new booking to the databse */
+    static async bookRoom(roomId, userId, booking) {
+
+        console.log("USER ID IS: " + userId)
+
+        const response = await axios.post(`${this.BASE_URL}/bookings/book-room/${roomId}/${userId}`, booking, {
+            headers: this.getHeader()
+        })
+        return response.data
+    }
+
+    /* Busca todas reservas */
+    static async getAllBookings() {
+        const result = await axios.get(`${this.BASE_URL}/bookings/all`, {
+            headers: this.getHeader()
+        })
+        return result.data
+    }
+
+    /* Busca uma reserva por cnfirmation code */
+    static async getBookingByConfirmationCode(bookingCode) {
+        const result = await axios.get(`${this.BASE_URL}/bookings/get-by-confirmation-code/${bookingCode}`)
+        return result.data
+    }
+
+    /* Cancelar reserva do usuário */
+    static async cancelBooking(bookingId) {
+        const result = await axios.delete(`${this.BASE_URL}/bookings/cancel/${bookingId}`, {
+            headers: this.getHeader()
+        })
+        return result.data
+    }
+
+
+    /**AUTHENTICATION CHECKER */
+    static logout() {
+        localStorage.removeItem('token')
+        localStorage.removeItem('role')
+    }
+
+    static isAuthenticated() {
+        const token = localStorage.getItem('token')
+        return !!token
+    }
+
+    static isAdmin() {
+        const role = localStorage.getItem('role')
+        return role === 'ADMIN'
+    }
+
+    static isUser() {
+        const role = localStorage.getItem('role')
+        return role === 'USER'
+    }
+}
